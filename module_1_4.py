5d3575bf80f8c6680975a1bb348d60690ab88bf7
arsenalio <arslanlev@gmail.com>
2024-10-22 15:11:22 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,11 @@
+my_string = 'это моя страна'
+print(len(my_string))
+up_string = my_string.upper()
+print(up_string)
+low_string = my_string.lower()
+print(low_string)
+my_string = my_string.replace(" ", "")
+print(my_string)
+print(my_string[0])
+print(my_string[-1])
+
